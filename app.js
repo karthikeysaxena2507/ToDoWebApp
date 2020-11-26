f84fad772e3661bc4746c5916294321676158c2c
Karthikey <karthikeysaxena@gmail.com>
2020-11-26 12:53:00 +0530

    changed uri

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/todo",
+    callbackURL: "https://todo-karthikey.herokuapp.com/todo-karthikey/auth/google/todo",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
